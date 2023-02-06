45f6a5175402c0de7ae724bb9567d850f7c5de03
locvd03052001 <91066718+locvd03052001@users.noreply.github.com>
2023-02-06 23:49:06 +0700

    Model config

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,16 @@
+const Course = require('../models/Courses');
+const { mutipleMongooseToObject } = require('../../ultil/mongooese');
+
 class SiteController {
   // [GET] /
-  index(req, res) {
-    res.render('home');
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render('home', {
+          courses: mutipleMongooseToObject(courses),
+        });
+      })
+      .catch((error) => next(error));
   }
 
   // [GET] /search
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
-const app = express();
-const port = 3000;
 
 const route = require('./routes');
+const db = require('../src/config/db');
+
+// Connect to DB
+db.connect();
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,10 +27,11 @@ app.use(express.json());
 // Template engine
 app.engine(
   'hbs',
-  handlebars.engine({
+  handlebars({
     extname: '.hbs',
   }),
 );
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
@@ -33,5 +39,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
