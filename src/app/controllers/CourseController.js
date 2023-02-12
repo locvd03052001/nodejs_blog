0b598cd3f833f0666da677cb129fc161d4c471b2
locvd03052001 <91066718+locvd03052001@users.noreply.github.com>
2023-02-12 19:54:29 +0700

    Create course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -2,6 +2,23 @@ const Course = require('../models/Courses');
 const { mongooseToObject } = require('../../ultil/mongooese');
 
 class CourseController {
+  // [GET] /courses/create
+  create(req, res, next) {
+    res.render('courses/create');
+  }
+
+  // [POST] /courses/store
+  store(req, res, next) {
+    const formDate = req.body;
+    formDate.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(formDate);
+
+    course
+      .save()
+      .then(() => res.redirect(`/`))
+      .catch((error) => {});
+  }
+
   // [GET] /courses/:slug
   show(req, res, next) {
     Course.findOne({ slug: req.params.slug })
