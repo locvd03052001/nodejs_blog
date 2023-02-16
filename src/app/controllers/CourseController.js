46c4f50cf2697e426bfaecba6b05680e72254704
locvd03052001 <91066718+locvd03052001@users.noreply.github.com>
2023-02-16 23:13:05 +0700

    function Delete Course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -47,6 +47,13 @@ class CourseController {
       .then(() => res.redirect('/me/stored/courses'))
       .catch((error) => next(error));
   }
+
+  // [DELETE] /courses/:id
+  destroy(req, res, next) {
+    Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch((error) => next(error));
+  }
 }
 
 module.exports = new CourseController();
